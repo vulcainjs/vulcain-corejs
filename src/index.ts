c6a26f43918fe39bdcd3c536120e932c03c7d411
Zenasoft <alain.metge@zenasoft.com>
2016-07-12 19:26:20 +0200

    remove account management

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 // Entry point
 import {DefaultServiceNames, RabbitAdapter, Application} from './core';
-import {AccountManagement} from './accountManagement';
 
 class MyApplication extends Application {
     initializeServices() {
         this.useRabbitAdapter('amqp://192.168.99.100');
-        AccountManagement.init(this.container);
     }
 }
 
