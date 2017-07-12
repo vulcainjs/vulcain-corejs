f4f2ffea028e8cbe9cda04bcd237a68ad08d72cc
METGE Alain <alain.metge@zenasoft.com>
2017-07-12 15:22:48 +0200

    reformat code

--- test/schema/validateData.ts
+++ test/schema/validateData.ts
@@ -1,55 +1,55 @@
-import {expect} from "chai";
-import {Model, Property, Reference, Validator, SchemaTypeDefinition, ISchemaTypeDefinition} from "../../dist/schemas/annotations";
-import {Domain} from "../../dist/schemas/schema";
+import { expect } from "chai";
+import { Model, Property, Reference, Validator, SchemaTypeDefinition, ISchemaTypeDefinition } from "../../dist/schemas/annotations";
+import { Domain } from "../../dist/schemas/schema";
 import 'mocha';
 import { SchemaStandardTypes } from "../../dist/schemas/standards";
 import { TestContext } from '../../dist/di/testContext';
 
 @Model()
 class BaseModel {
-    @Property({type: "string", required: true})
-    @Validator("length", {min: 2})
+    @Property({ type: "string", required: true })
+    @Validator("length", { min: 2 })
     baseText: string;
 }
 
-@Model({extends: "BaseModel"})
+@Model({ extends: "BaseModel" })
 class SimpleModel extends BaseModel {
-    @Property({ type: 'string', required: true})
+    @Property({ type: 'string', required: true })
     text: string;
-    @Property({type:'number'})
+    @Property({ type: 'number' })
     number: number;
 }
 
 @Model()
 class ReferenceModel {
-    @Reference({item: "SimpleModel", cardinality: "one", required: true})
+    @Reference({ item: "SimpleModel", cardinality: "one", required: true })
     simple: SimpleModel;
-    @Reference({item: "SimpleModel", cardinality: "many"})
+    @Reference({ item: "SimpleModel", cardinality: "many" })
     multiples: Array<SimpleModel>;
 }
 
 
 @Model()
 class EmailModel {
-    @Property({type: SchemaStandardTypes.email})
+    @Property({ type: SchemaStandardTypes.email })
     email: string;
 }
 
 @Model()
 class UrlModel {
-    @Property({type: "url"})
+    @Property({ type: "url" })
     url: string;
 }
 
 @Model()
 class AlphanumericModel {
-    @Property({type: "alphanumeric"})
+    @Property({ type: "alphanumeric" })
     value: string;
 }
 
 @Model()
 class DateIsoModel {
-    @Property({type: "date-iso8601"})
+    @Property({ type: "date-iso8601" })
     date: string;
 }
 
@@ -60,7 +60,7 @@ export class ArrayOfEnum implements ISchemaTypeDefinition {
 
     // Type properties
     messages: string[];
-    
+
     // Initialize properties
     constructor() {
         this.messages = [
@@ -68,8 +68,9 @@ export class ArrayOfEnum implements ISchemaTypeDefinition {
             "Invalid value '{$value}' for '{$propertyName}', value must be an array."
         ];
     }
+
     validate(val) {
-        if (!this.$values) return "You must define array item type with the 'items' property.";
+        if (!this.$values) return "You must define array item enumeration values with the 'values' property.";
         if (!Array.isArray(val)) return this.messages[1];
         let error = false;
         val.forEach(e => {
@@ -83,7 +84,7 @@ export class ArrayOfEnum implements ISchemaTypeDefinition {
 
 @Model()
 class ArrayOfModel {
-    @Property({type: "ArrayOfEnum", items: "string", values: ["a", "b"]})
+    @Property({ type: "ArrayOfEnum", items: "string", values: ["a", "b"] })
     enums: string[];
 }
 
@@ -91,38 +92,38 @@ let context = new TestContext();
 
 describe("Validate data", function () {
 
-    it("should validate base class", async() => {
+    it("should validate base class", async () => {
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model: SimpleModel = {text: "text", number: 1, baseText: ""};
+        let model: SimpleModel = { text: "text", number: 1, baseText: "" };
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("baseText");
     });
 
-    it("should validate call validator", async() => {
+    it("should validate call validator", async () => {
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model: SimpleModel = {text: "text", number: 1, baseText: "a"};
+        let model: SimpleModel = { text: "text", number: 1, baseText: "a" };
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("baseText");
     });
 
-    it("should validate malformed number", async() => {
+    it("should validate malformed number", async () => {
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model = schema.bind({text: "text", number: "1w1", baseText: "text"});
+        let model = schema.bind({ text: "text", number: "1w1", baseText: "text" });
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("number");
     });
 
-    it("should validate valid values", async() => {
-        let model: SimpleModel = {text: "text", number: 1, baseText: "text"};
+    it("should validate valid values", async () => {
+        let model: SimpleModel = { text: "text", number: 1, baseText: "text" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
         let errors = await schema.validateAsync(null, model);
@@ -130,7 +131,7 @@ describe("Validate data", function () {
         expect(errors.length).equals(0);
     });
 
-    it("should validate values in reference", async() => {
+    it("should validate values in reference", async () => {
         let model: any = { text: "text", number: "1M", baseText: "text" };
         let refs = { simple: model };
 
@@ -141,7 +142,7 @@ describe("Validate data", function () {
         expect(errors.length).equals(1);
     });
 
-    it("should validate invalid multiple references", async() => {
+    it("should validate invalid multiple references", async () => {
         let model: any = { text: "text", number: "1M", baseText: "text" };
         let refs = { simple: model, multiples: model };
 
@@ -152,7 +153,7 @@ describe("Validate data", function () {
         expect(errors.length).equals(2);
     });
 
-    it("should validate values in multiple references", async() => {
+    it("should validate values in multiple references", async () => {
         let model: any = { text: "text", number: "1M", baseText: "text" };
         let refs = { simple: model, multiples: [model, model] };
 
@@ -163,8 +164,8 @@ describe("Validate data", function () {
         expect(errors.length).equals(3);
     });
 
-    it("should validate required reference", async() => {
-        let refs = { };
+    it("should validate required reference", async () => {
+        let refs = {};
 
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("ReferenceModel");
@@ -175,18 +176,18 @@ describe("Validate data", function () {
 
     // ---------------
     // email
-    it('should validate email value', async() => {
+    it('should validate email value', async () => {
 
-        let model: EmailModel = {email: "first.name@email.com"};
+        let model: EmailModel = { email: "first.name@email.com" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("EmailModel");
         let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(0, 'The email is malformed');
     });
-    it('should validate malformed email value', async() => {
+    it('should validate malformed email value', async () => {
 
-        let model: EmailModel = {email: "first.name@email"};
+        let model: EmailModel = { email: "first.name@email" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("EmailModel");
         let errors = await schema.validateAsync(null, model);
@@ -197,18 +198,18 @@ describe("Validate data", function () {
 
     // ---------------
     // url
-    it('should validate url value', async() => {
+    it('should validate url value', async () => {
 
-        let model: UrlModel = {url: "https://myWebsite.com/#ancre/1"};
+        let model: UrlModel = { url: "https://myWebsite.com/#ancre/1" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("UrlModel");
         let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(0);
     });
-    it('should validate malformed url value', async() => {
+    it('should validate malformed url value', async () => {
 
-        let model: UrlModel = {url: "http://site.r"};
+        let model: UrlModel = { url: "http://site.r" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("UrlModel");
         let errors = await schema.validateAsync(null, model);
@@ -218,18 +219,18 @@ describe("Validate data", function () {
 
     // ---------------
     // Alphanumeric
-    it('should validate alphanumeric value', async() => {
+    it('should validate alphanumeric value', async () => {
 
-        let model: AlphanumericModel = {value: "abcde1345fghik6789"};
+        let model: AlphanumericModel = { value: "abcde1345fghik6789" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("AlphanumericModel");
         let errors = await schema.validateAsync(undefined, model);
 
         expect(errors.length).equals(0);
     });
-    it('should validate malformed alphanumeric value', async() => {
+    it('should validate malformed alphanumeric value', async () => {
 
-        let model: AlphanumericModel = {value: "abc123!"};
+        let model: AlphanumericModel = { value: "abc123!" };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("AlphanumericModel");
         let errors = await schema.validateAsync(undefined, model);
@@ -239,9 +240,9 @@ describe("Validate data", function () {
 
     // ---------------
     // Date ISO86
-    it('should validate date ISO8601', async() => {
+    it('should validate date ISO8601', async () => {
 
-        let model: DateIsoModel = {date: new Date().toISOString()};
+        let model: DateIsoModel = { date: new Date().toISOString() };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("DateIsoModel");
         let errors = await schema.validateAsync(undefined, model);
@@ -250,9 +251,9 @@ describe("Validate data", function () {
     });
 
 
-    it('should validate malformed date ISO8601', async() => {
+    it('should validate malformed date ISO8601', async () => {
 
-        let model: DateIsoModel = {date: new Date().toDateString()};
+        let model: DateIsoModel = { date: new Date().toDateString() };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("DateIsoModel");
         let errors = await schema.validateAsync(undefined, model);
@@ -262,9 +263,9 @@ describe("Validate data", function () {
 
     // ---------------------
     // ArrayOf enum
-    it('should validate array of enum', async() => {
+    it('should validate array of enum', async () => {
 
-        let model: ArrayOfModel = { enums: ["a", "b"] };
+        let model: ArrayOfModel = { enums: ["a", "bb"] };
         let domain = context.rootContainer.get<Domain>("Domain");
         let schema = domain.getSchema("ArrayOfModel");
         let errors = await schema.validateAsync(undefined, model);
