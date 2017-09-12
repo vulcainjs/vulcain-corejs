30f9b349a05e771b4b07170d8763f133c646a0de
malain <alain.metge@zenasoft.com>
2017-09-12 23:21:06 +0200

    init

--- src/configurations/index.ts
+++
@@ -1,7 +0,0 @@
-export { AbstractRemoteSource } from './sources/abstractRemoteSource';
-export { FileConfigurationSource, ConfigurationDataType } from './sources/fileConfigurationSource';
-export { MemoryConfigurationSource } from './sources/memoryConfigurationSource';
-export { HttpConfigurationSource } from './sources/httpConfigurationSource';
-export { ILocalConfigurationSource,DataSource, IDynamicProperty, IRemoteConfigurationSource } from './abstractions';
-export { ConfigurationSourceBuilder } from './configurationSourceBuilder';
-export { DynamicConfiguration } from './dynamicConfiguration';
